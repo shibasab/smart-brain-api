8bb070143689878690273e3b1832d5ed6d3cc01a
shibasab <34082145+shibasab@users.noreply.github.com>
2019-09-19 11:44:16 +0900

    heroku first-commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+const saltRounds = 10;
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'test',
+    database: "'smart-brain'"
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  db.select('*')
+    .from('users')
+    .then(user => {
+      if (user.length) {
+        res.json(user);
+      } else {
+        res.status(400).json('Not found');
+      }
+    })
+    .catch(err => {
+      status(400).json('error getting user');
+    });
+});
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+});
